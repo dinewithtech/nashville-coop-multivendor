3daee9f98984164f196a6e8276a85f5a47b1af30
faizan tayyab <faizantayyab37@gmail.com>
2026-02-10 14:38:40 +0500

    feat: Update app branding to Nashville Coop and refresh the primary color palette.

--- nashville-coop-store/lib/utils/constants/colors.ts
+++ nashville-coop-store/lib/utils/constants/colors.ts
@@ -5,20 +5,20 @@
 
 export const Colors = {
   light: {
-    primary: "#B83330",
+    primary: "#f50000",
     themeBackground: "#FAFAFA",
     iconColor: "#333333",
-    tagColor: "#B83330",
-    iconPink: "#B83330",
+    tagColor: "#f50000",
+    iconPink: "#f50000",
     radioColor: "#FFF",
-    radioOuterColor: "#B83330",
-    spinnerColor: "#B83330",
+    radioOuterColor: "#f50000",
+    spinnerColor: "#f50000",
     orderComplete: "#89a207",
     orderUncomplete: "#fe0000",
     horizontalLine: "#B8B8B8",
-    buttonBackground: "#B83330",
+    buttonBackground: "#f50000",
     buttonText: "black",
-    buttonBackgroundPink: "#B83330",
+    buttonBackgroundPink: "#f50000",
     buttonTextPink: "#FFF",
     textErrorColor: "#FA7751",
     headerBackground: "#FFF",
@@ -26,12 +26,12 @@ export const Colors = {
     fontMainColor: "#212121",
     fontSecondColor: "#949393",
     cartContainer: "#FFF",
-    startColor: "#B83330",
+    startColor: "#f50000",
     white: "#FFF",
     black: "#000",
     tabNaviatorBackground: "#1F2937",
     secondaryTextColor: "#4B5563",
-    lowOpacityPrimaryColor: "#E0A896",
+    lowOpacityPrimaryColor: "#FFE5E5",
     mainTextColor: "#000000",
     switchButtonColor: "#4F46E5",
     sidebarIconBackground: "#E5E7EB",
@@ -42,15 +42,15 @@ export const Colors = {
     error: "#ef4444",
   },
   dark: {
-    primary: "#B83330", // Slightly darker green to avoid too much brightness
+    primary: "#f50000", // Slightly darker green to avoid too much brightness
     themeBackground: "black", // Dark gray for background
     screenBackground: "black",
     iconColor: "black", // Light gray for icons
     tagColor: "#8CA30D", // Muted green for tags
     iconPink: "#8CA30D",
     radioColor: "#1E1E1E", // Matches the dark background
-    radioOuterColor: "#B83330",
-    spinnerColor: "#B83330",
+    radioOuterColor: "#f50000",
+    spinnerColor: "#f50000",
     orderComplete: "#6F8B05", // Muted green
     orderUncomplete: "#FF6B6B", // Softer red for dark mode
     horizontalLine: "#444444", // Dark gray for dividers
@@ -68,7 +68,7 @@ export const Colors = {
     white: "#E0E0E0", // Light gray instead of pure white
     black: "#000",
     secondaryTextColor: "#4B5563",
-    lowOpacityPrimaryColor: "#E0A896",
+    lowOpacityPrimaryColor: "#FFE5E5",
     mainTextColor: "#f2f2f2",
     switchButtonColor: "#4F46E5",
     sidebarIconBackground: "#E5E7EB",
